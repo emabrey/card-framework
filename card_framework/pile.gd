3724698dbd40387b9c6d6963221f9ef4840e9d3c
chun92 <chun92@postech.ac.kr>
2024-12-09 23:37:53 +0900

    refactor: refactor pile.gd

--- card_framework/pile.gd
+++ card_framework/pile.gd
@@ -26,24 +26,28 @@ func get_top_card() -> Card:
 
 func shuffle():
 	_held_cards.shuffle()
-	for i in _held_cards.size():
+	for i in range(_held_cards.size()):
 		var card = _held_cards[i]
 		cards.move_child(card, i)
 	_update_target_positions()
 
 
 func _update_target_z_index():
-	for i in _held_cards.size():
+	for i in range(_held_cards.size()):
 		var card = _held_cards[i]
 		card.stored_z_index = PILE_Z_INDEX + i if card.is_clicked else i
 
 
 func _update_target_positions():
-	var last_offset: Vector2
-	for i in _held_cards.size():
+	var last_index = _held_cards.size() - 1
+	var last_offset = _calculate_offset(last_index)
+	if enable_drop_zone and align_drop_zone_with_top_card:
+		drop_zone.change_sensor_position_with_offset(last_offset)
+		drop_zone.change_placement_position_with_offset(last_offset)
+
+	for i in range(_held_cards.size()):
 		var card = _held_cards[i]
 		var offset = _calculate_offset(i)
-		last_offset = offset
 		var target_pos = position + offset
 		card.show_front = card_face_up
 		card.move_rotation(0)
@@ -56,10 +60,6 @@ func _update_target_positions():
 				card.can_be_interact_with = true
 			else:
 				card.can_be_interact_with = false
-	
-	if enable_drop_zone and align_drop_zone_with_top_card:
-		drop_zone.change_sensor_position_with_offset(last_offset)
-		drop_zone.change_placement_position_with_offset(last_offset)
 
 
 func _calculate_offset(index: int) -> Vector2:
