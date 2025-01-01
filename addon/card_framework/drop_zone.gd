db8819e317c30481a52858a4f685d33ac264de75
chun92 <chun92@postech.ac.kr>
2025-01-02 00:38:30 +0900

    refactor: correct variable declarations and parameter names in DropZone class

--- addon/card_framework/drop_zone.gd
+++ addon/card_framework/drop_zone.gd
@@ -1,24 +1,24 @@
 class_name DropZone
 extends Control
 
-var sensor_size: Vector2 : 
+
+var sensor_size: Vector2: 
 	set(value):
 		sensor.size = value
-var sensor_position: Vector2 : 
+var sensor_position: Vector2: 
 	set(value):
 		sensor.position = value
-var sensor_texture : Texture :
+var sensor_texture : Texture:
 	set(value):
 		sensor_holder.texture = value
-var sensor_visible := true :
+var sensor_visible := true:
 	set(value):
 		sensor.visible = value
-
 var stored_sensor_position: Vector2
 var parent_card_container: CardContainer
-
 var sensor: Control
 var sensor_holder: Control
+
 
 func check_mouse_is_in_drop_zone() -> bool:
 	var mouse_position = get_global_mouse_position()
@@ -26,7 +26,7 @@ func check_mouse_is_in_drop_zone() -> bool:
 	return result
 
 
-func set_sensor(_size: Vector2, _positon: Vector2, _texture: Texture, _visible: bool):
+func set_sensor(_size: Vector2, _position: Vector2, _texture: Texture, _visible: bool):
 	if sensor == null:
 		sensor = TextureRect.new()
 		sensor.name = "Sensor"
@@ -41,14 +41,15 @@ func set_sensor(_size: Vector2, _positon: Vector2, _texture: Texture, _visible: 
 		sensor_holder.z_index = -1000
 		add_child(sensor_holder)
 		sensor_holder.size = _size
-		sensor_holder.position = _positon
+		sensor_holder.position = _position
 		sensor_holder.texture = _texture
 		sensor_holder.visible = _visible
 		
 	sensor_size = _size
-	sensor_position = _positon
-	stored_sensor_position = _positon
+	sensor_position = _position
+	stored_sensor_position = _position
 	sensor_visible = _visible
+
 
 func change_sensor_position_with_offset(offset: Vector2):
 	sensor_position = stored_sensor_position + offset
