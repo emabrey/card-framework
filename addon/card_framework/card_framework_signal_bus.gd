255b4f062a7bb8be380f064acff139f25079b5b9
Chun <chunuiyu@gmail.com>
2025-01-01 16:55:30 +0900

    refactor: remove unused signal for card movement completion

--- addon/card_framework/card_framework_signal_bus.gd
+++ addon/card_framework/card_framework_signal_bus.gd
@@ -1,15 +1,10 @@
 extends Node
 
 signal drag_dropped(cards: Array)
-signal card_move_done(card: Card)
 
 
 func _ready():
 	drag_dropped.connect(_on_drag_dropped)
-	card_move_done.connect(_on_card_move_done)
 
 func _on_drag_dropped(_cards: Array):
-	pass
-
-func _on_card_move_done(_card: Card):
 	pass
